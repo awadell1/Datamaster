4f473f1e0c171c6973e4bdd767b08dbd9683ec04
Alexius Wadell <awadell@gmail.com>
2016-10-11 23:19:01 -0400

    Added a default config file
    Use config.ini unless it doesn't exist then use default.ini
    
    Added warning to reportGitInfo if git is not being used

--- reportGitInfo.m
+++ reportGitInfo.m
@@ -1,6 +1,10 @@
 function reportGitInfo()
     %Report Current Version info
     gitInfo = getGitInfo(fileparts(which('reportGitInfo')));
-    fprintf('Version: %s - %s\n',gitInfo.branch, gitInfo.hash);
-    fprintf('Host: %s\n',gitInfo.url);
+    if ~isempty(gitInfo)
+    	fprintf('Version: %s - %s\n',gitInfo.branch, gitInfo.hash);
+    	fprintf('Host: %s\n',gitInfo.url);
+    else
+    	warning('You are not using git and will miss out on the latest updates');
+    end
 end
