e7abc0adbb207393575de5f78fad86cf87fcf91d
Alexius Wadell <awadell@gmail.com>
2016-10-17 18:02:26 -0400

    Switched to using python to connect to SQLite database
    -Some computers were unable to use mksqlite due to hardware config
    -Requiring users to compile mex code is a high bar while installing python
    is not
    -Requiring python will open up the use of other packages to supplment the
    matlab code base

---
+++ Filters/SteadyStateDetection.m
@@ -0,0 +1,8 @@
+function [ output_args ] = SteadyStateDetection(channel, timeWindow, alpha)
+    %UNTITLED Summary of this function goes here
+    %   Detailed explanation goes here
+    
+    
+    
+end
+
