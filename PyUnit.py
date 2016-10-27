102dc2317d5761a9fb8bd0e57ef54fb41b502ab6
Alexius Wadell <awadeel@gmail.com>
2016-10-27 16:48:06 -0400

    Updated PyUnits to handle the degree symbol and dimensionless units
    Also updated formating option for units to better represent the results

--- PyUnit.py
+++ PyUnit.py
@@ -4,19 +4,25 @@ from pint import UnitRegistry
 ureg = UnitRegistry(); #Load the Default Unit Definitions
 ureg.load_definitions('UnitDefine.txt') #Load additonal definitions
 
-def convertUnit(value, oldUnit, newUnit='base'):
-	#Get Quanity Object
-	valueOld = UnitRegistry().Quantity(numpy.array(value), oldUnit)
+def convertUnit(value, oldUnit='', newUnit='base'):
+	
 
-	if oldUnit == '':
+	if not oldUnit:
 		#Dimensionless -> Return
 		return {'value':value, 'unit':''}
-	elif newUnit == 'base':
-		#Convert to base unit
-		valueNew = valueOld.to_base_units()
 	else:
-		#Convert to New Unit
-		valueNew = valueOld.to(newUnit)
+		# Replace the degree symbol with 'deg'
+		oldUnit = oldUnit.decode("utf-8").replace(u"\u00B0", "deg")
+
+		#Get Quanity Object
+		valueOld = UnitRegistry().Quantity(numpy.array(value), oldUnit)
+
+		if newUnit == 'base':
+			#Convert to base unit
+			valueNew = valueOld.to_base_units()
+		else:
+			#Convert to New Unit
+			valueNew = valueOld.to(newUnit)
 
 	#Return as list of magnitude
-	return {'value':valueNew.magnitude.tolist(), 'unit':'{:~}'.format(valueNew.units)}
+	return {'value':valueNew.magnitude.tolist(), 'unit':'{}'.format(valueNew.units)}
